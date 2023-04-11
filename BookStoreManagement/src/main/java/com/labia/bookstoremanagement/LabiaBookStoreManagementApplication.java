288424e589c79db54ad90e5e265590a6ec7ecfc7
Gruxeon2507 <87994534+Gruxeon2507@users.noreply.github.com>
2023-04-11 11:42:25 +0700

    Initialize Project

---
+++ BookStoreManagement/src/main/java/com/labia/bookstoremanagement/LabiaBookStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.labia.bookstoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabiaBookStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabiaBookStoreManagementApplication.class, args);
+	}
+
+}
