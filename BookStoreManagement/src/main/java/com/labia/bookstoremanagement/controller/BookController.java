b233bbd4cc8a2df13ca49cacad9d00e968dca510
Gruxeon2507 <87994534+Gruxeon2507@users.noreply.github.com>
2023-04-17 22:05:28 +0700

    add api:  get pdf and cover for each book

--- BookStoreManagement/src/main/java/com/labia/bookstoremanagement/controller/BookController.java
+++ BookStoreManagement/src/main/java/com/labia/bookstoremanagement/controller/BookController.java
@@ -6,8 +6,17 @@ package com.labia.bookstoremanagement.controller;
 
 import com.labia.bookstoremanagement.model.Book;
 import com.labia.bookstoremanagement.repository.BookRepository;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author ADMIN
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/book")
 public class BookController {
@@ -41,5 +51,33 @@ public class BookController {
         return bookRepository.getBookByCategoryId(categoryId);
     }
     
+    @GetMapping(value = "/pdf/{fileId}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> getFile(@PathVariable String fileId) throws IOException {
+            String filePath = "pdf/" + fileId +".pdf";
+        File file = new File(filePath);
+        InputStream inputStream = new FileInputStream(file);
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + fileId);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(inputStreamResource);
+    }
+    
+    @GetMapping(value = "/image/{fileId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> getImage(@PathVariable String fileId) throws IOException {
+    String filePath = "cover/" + fileId +".jpg";
+    File file = new File(filePath);
+    InputStream inputStream = new FileInputStream(file);
+    InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+    HttpHeaders headers = new HttpHeaders();
+    headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + fileId);
+    return ResponseEntity.ok()
+            .headers(headers)
+            .contentType(MediaType.IMAGE_JPEG)
+            .body(inputStreamResource);
+}
+
 
 }
