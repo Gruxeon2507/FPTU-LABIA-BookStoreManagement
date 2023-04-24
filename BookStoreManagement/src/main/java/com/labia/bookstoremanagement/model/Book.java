3e83f41c5d00db5ac2b2bbb38418c943d7efe2c6
hoenntk68 <hn8319542@gmail.com>
2023-04-24 19:03:58 +0700

    add description attribute for Book

--- BookStoreManagement/src/main/java/com/labia/bookstoremanagement/model/Book.java
+++ BookStoreManagement/src/main/java/com/labia/bookstoremanagement/model/Book.java
@@ -43,6 +43,7 @@ public class Book implements Serializable {
     private int noSale;
     private int noView;
     private String authorName;
+    private String description;
 
     @ManyToOne
     @JoinColumn(name = "createdBy", referencedColumnName = "username")
