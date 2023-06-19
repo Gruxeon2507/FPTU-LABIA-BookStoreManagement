203e4d0e4f924b8c83c9af771ed44abe4f0b6de2
duckmhe170996 <khieuminhduc2012@gmail.com>
2023-06-19 15:20:51 +0700

    add services for UUID

--- BookStoreManagement/src/main/java/com/labia/bookstoremanagement/controller/PathTraversalController.java
+++ BookStoreManagement/src/main/java/com/labia/bookstoremanagement/controller/PathTraversalController.java
@@ -2,6 +2,7 @@ package com.labia.bookstoremanagement.controller;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +18,21 @@ import java.io.InputStream;
 @RequestMapping("api/pathvul/")
 public class PathTraversalController {
     @GetMapping (value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<InputStreamResource> getFile(@RequestParam("filename") String fileId, HttpServletRequest request) throws IOException {
-        String filePath = "pdf/" + fileId + ".pdf";
-        File file = new File(filePath);
-        InputStream inputStream = new FileInputStream(file);
-        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + fileId);
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(inputStreamResource);
+    public ResponseEntity<?> getFile(@RequestParam("filename") String fileId, HttpServletRequest request) throws IOException {
+        try{
+            String filePath = "pdf/" + fileId + ".pdf";
+            File file = new File(filePath);
+            InputStream inputStream = new FileInputStream(file);
+            InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + fileId);
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(inputStreamResource);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
     }
 }
